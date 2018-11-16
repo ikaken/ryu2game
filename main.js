fbd61c86c5e2c7e56db848c1829c39631a8a2a26
ikaken <user@ikaken.com>
2018-11-16 21:54:43 +0900

    up

--- main.js
+++ main.js
@@ -8,53 +8,49 @@ window.onload = function(){
     game.fps = 15;
 
 
-    game.preload("img/chara1.png");
-    game.preload("img/chara2.png");
-    game.preload("img/chara3.png");
-    game.preload("img/f1.png");
+    game.preload("img/chara1.png","img/chara2.png","img/chara3.png","img/フレーム.png","img/背景.png","img/テキストエリア.png");
 
  
      game.onload = function(){
 
-        bear = new Sprite(32, 32);
         c1 = new Sprite(720, 1220);
-        f1 = new Sprite(1240, 1754);
+        f1 = new Sprite(1080, 1920);
+        b1 = new Sprite(1080, 1920);
+        t1 = new Sprite(1080, 570);
 
-        bear.image = game.assets["img/chara1.png"];
         c1.image = game.assets["img/chara2.png"];
-        f1.image = game.assets["img/f1.png"];
+        f1.image = game.assets["img/フレーム.png"];
+        b1.image = game.assets["img/背景.png"];
+        t1.image = game.assets["img/テキストエリア.png"];
 
-        bear.x = 0;
-        bear.y = 100;
+
 
         c1.x = 100;
-        c1.y = 100;
-        
+        c1.y = 300;
 
-        bear.frame = 5;
+        c1.frame = 5;
 
+        t1.y = 1220;
+
+        game.rootScene.addChild(b1);
         game.rootScene.addChild(c1);
-        game.rootScene.addChild(bear);
+        game.rootScene.addChild(t1);
         game.rootScene.addChild(f1);
+
 
         var myLabel = new Label("モンスターが現れた！");
         myLabel.font = "50px Palatino";
-        myLabel.x = 200;	// X座標
-        myLabel.y = 1250;	// Y座標
+        myLabel.x = 100;	// X座標
+        myLabel.y = 1300;	// Y座標
+        myLabel.color = "rgb(255, 255, 255)";
+        myLabel.width = 1080;
+        myLabel.height = 570;
         // ラベルを画面に表示
         game.rootScene.addChild(myLabel);
 
 
-        bear.addEventListener("enterframe", function(){
-            this.x += 1;
-
-            this.frame = this.age % 2 + 6;
-        });
 
 
-        bear.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bear);
-        });
     };
 
     game.start();
