a7a834cacff4534a2f2edec1b3f8ef694a7f6d48
ikaken <user@ikaken.com>
2019-01-31 13:57:29 +0900

    up

--- main.js
+++ main.js
@@ -2,6 +2,111 @@ enchant();
 
 
 
+var Player = enchant.Class.create(enchant.Sprite, {
+  initialize: function (core_) {
+    enchant.Sprite.call(this, 192, 256);
+    this.x = 700;
+    this.y = 500;
+    this.image = core_.assets['img/主人公s.png'];
+    this.frame = 5;
+  },
+  attackTo: function (target) {
+    console.log("attackTo");
+    this.tl.moveTo(400, 500, 10);
+    this.tl.moveTo(400, 510, 1);
+    this.tl.moveTo(400, 500, 1);
+    this.tl.moveTo(400, 490, 1);
+    this.tl.moveTo(400, 500, 1);
+
+    this.tl.moveTo(400, 510, 1);
+    this.tl.moveTo(400, 500, 1);
+    this.tl.moveTo(400, 490, 1);
+    this.tl.moveTo(400, 500, 1);
+
+    this.tl.then(function () {
+      target.tl.moveTo(290, 500, 1);
+      target.tl.moveTo(300, 500, 1);
+      target.damageCheck(15);
+    });
+
+    this.tl.moveTo(this.x, this.y, 10);
+  }
+  
+});
+
+
+
+var enemy = enchant.Class.create(enchant.Sprite, {
+  initialize: function (core_) {
+    enchant.Sprite.call(this, 192, 256);
+    this.x = 300;
+    this.y = 500;
+    this.image = core_.assets['img/モンスター1s.png'];
+    this.frame = 5;
+    this.hp = 100;
+  },
+  attackTo: function (target) {
+    console.log("attackTo");
+    this.tl.moveTo(700, 500, 10);
+    this.tl.moveTo(700, 510, 1);
+    this.tl.moveTo(700, 500, 1);
+    this.tl.moveTo(700, 490, 1);
+    this.tl.moveTo(700, 500, 1);
+
+    this.tl.moveTo(700, 510, 1);
+    this.tl.moveTo(700, 500, 1);
+    this.tl.moveTo(700, 490, 1);
+    this.tl.moveTo(700, 500, 1);
+
+    this.tl.then(function () {
+      target.tl.moveTo(target.x-10, target.y, 1);
+      target.tl.moveTo(target.x, target.y, 1);
+      target.damageCheck(15);
+    });
+
+    this.tl.moveTo(this.x, this.y, 10);
+  },
+  damageCheck : function (attackPoint) {
+    console.log("damageCheck");
+    this.hp -= attackPoint;
+    console.log(this.hp); //hp表示
+
+    if (this.hp > 0) {
+      let whosay = new Label("モンスターは" + attackPoint + "のダメージを受けた。");
+      whosay.font = "50px monospace";
+      whosay.color = "rgb(255, 255, 255)";
+      whosay.y = 1300;
+      whosay.x = 100;
+      whosay.width = 1080 - 200;
+      whosay.height = 570;
+      whosay.opacity = 0;
+      whosay.tl.fadeIn(15);
+  
+      textLayer.addChild(whosay);
+    } else {
+      let whosay = new Label("モンスターを倒した。");
+      whosay.font = "50px monospace";
+      whosay.color = "rgb(255, 255, 255)";
+      whosay.y = 1300;
+      whosay.x = 100;
+      whosay.width = 1080 - 200;
+      whosay.height = 570;
+      whosay.opacity = 0;
+      whosay.tl.fadeIn(15);
+  
+      textLayer.addChild(whosay);
+
+      imageLayer.removeChild(this);
+
+    }
+
+
+  }
+  
+});
+
+
+
 var GoScene;
 
 
@@ -38,7 +143,60 @@ bgmlist = ["sound/bgm_maoudamashii_fantasy14.mp3","sound/game_maoudamashii_5_tow
     // return getNextSceneName();
   }
 
+  GoBattleScene = function() {
+    //テキストと次へボタンを削除する
+    textLayer.removeChild(textLayer.firstChild);
+    textLayer.removeChild(textLayer.firstChild);
+    textLayer.removeChild(textLayer.lastChild);
+    textLayer.removeChild(textLayer.lastChild);
+    touchLayer.removeChild(touchLayer.firstChild);
+    touchLayer.removeChild(touchLayer.firstChild);
+    touchLayer.removeChild(touchLayer.firstChild);
+    touchLayer.removeChild(touchLayer.firstChild);
 
+
+    var charaImage2 = new Sprite(172, 64);
+    charaImage2.image = core.assets["img/kougeki.png"];
+    charaImage2.x = 700;
+    charaImage2.y = 1100;
+    charaImage2.opacity = 0;
+    charaImage2.tl.fadeIn(8);
+    touchLayer.addChild(charaImage2);
+
+
+    //プレーヤーの生成表示
+    var Player01 = new Player(core);
+    imageLayer.addChild(Player01);
+
+    //敵のの生成表示
+    var enemy01 = new enemy(core);
+    imageLayer.addChild(enemy01);
+
+
+    //攻撃ボタンタップ
+    charaImage2.addEventListener('touchstart', function (e) {
+
+      textLayer.removeChild(textLayer.firstChild);
+      textLayer.removeChild(textLayer.firstChild);
+      textLayer.removeChild(textLayer.lastChild);
+      textLayer.removeChild(textLayer.lastChild);
+
+
+      Player01.attackTo(enemy01);
+//      enemy01.attackTo(Player01);
+      
+
+
+    });
+
+
+
+
+
+
+
+
+  }
 
     core.fps = 15;
 
@@ -89,6 +247,9 @@ bgmlist = ["sound/bgm_maoudamashii_fantasy14.mp3","sound/game_maoudamashii_5_tow
 
         //_ss.SetText(['モンスターが現れた！','違った、主人公だったｗ','間違えんなよ！']);
         
+
+
+
 
 
     };
